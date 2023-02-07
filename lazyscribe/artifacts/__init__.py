636825e7f8a4e2ae44ac338c275ccde65270f837
Akshay Gupta <akgcodes@gmail.com>
2023-02-07 00:16:26 -0500

    [WIP] Completely untested. Added in the actual logging and loading functionality to the experiment class

--- lazyscribe/artifacts/__init__.py
+++ lazyscribe/artifacts/__init__.py
@@ -6,4 +6,27 @@ from .base import Artifact
 from .json import JSONArtifact
 from .sklearn import SklearnArtifact
 
-__all__: List[str] = ["Artifact", "JSONArtifact", "SklearnArtifact"]
+__all__: List[str] = ["Artifact", "JSONArtifact", "SklearnArtifact", "_get_handler"]
+
+
+def _get_handler(alias: str) -> Artifact:
+    """Retrieve a specific handler based on the alias.
+
+    Parameters
+    ----------
+    alias : str
+        The alias for the handler.
+
+    Returns
+    -------
+    Artifact
+        The artifact handler.
+    """
+    for obj in Artifact.__subclasses__():
+        if obj.alias == alias:
+            out = obj
+            break
+    else:
+        raise ValueError(f"No handler available with the alias {alias}")
+
+    return out
