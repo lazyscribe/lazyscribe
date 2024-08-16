27813bdac1141a2713ed04345179ca78a601b82a
Gladys Teh <97971054+gladysteh99@users.noreply.github.com>
2024-08-16 14:34:13 -0400

    Dynamic artifact handling (#56)
    * refactor gethandler
    
    * remove old code
    
    * modify error message
    
    * address comments and add mock test for error
    
    * Removing the test artifact handler from the pyproject.toml
    
    * Writing documentation for custom artifact handlers
    
    ---------
    
    Co-authored-by: yteh7 <yteh7@gatech.edu>
    Co-authored-by: Akshay Gupta <akshay.gupta2@capitalone.com>

--- lazyscribe/artifacts/__init__.py
+++ lazyscribe/artifacts/__init__.py
@@ -2,11 +2,14 @@
 
 from typing import List, Type
 
-from lazyscribe.artifacts.base import Artifact
-from lazyscribe.artifacts.joblib import JoblibArtifact
-from lazyscribe.artifacts.json import JSONArtifact
+try:
+    from importlib_metadata import entry_points
+except ImportError:
+    from importlib.metadata import entry_points  # type: ignore
 
-__all__: List[str] = ["Artifact", "JSONArtifact", "JoblibArtifact", "_get_handler"]
+from lazyscribe.artifacts.base import Artifact
+
+__all__: List[str] = ["_get_handler"]
 
 
 def _get_handler(alias: str) -> Type[Artifact]:
@@ -23,11 +26,32 @@ def _get_handler(alias: str) -> Type[Artifact]:
         The artifact handler class object. This object will need to be constructed
         using :py:meth:`lazyscribe.artifacts.Artifact.construct`.
     """
-    for obj in Artifact.__subclasses__():
-        if obj.alias == alias:
-            out = obj
-            break
-    else:
-        raise ValueError(f"No handler available with the alias {alias}")
+    entry = entry_points(group="lazyscribe.artifact_type")
 
-    return out
+    for full_artifact_class in entry:  # search through entrypoints first
+        if full_artifact_class.name == alias:
+            try:
+                mod = full_artifact_class.load()
+            except ImportError as imp:
+                raise RuntimeError(
+                    f"Unable to import handler for {alias} through entry points"
+                ) from imp
+
+            if not isinstance(mod, type):
+                raise TypeError(f"{full_artifact_class} is not a class")
+
+            break
+
+    else:
+        for obj in Artifact.__subclasses__():  # search through experiment subclasses
+            if obj.alias == alias:
+                mod = obj
+                break
+
+        # no handler found in both entrypoints or subclass
+        else:
+            raise ValueError(
+                f"No handler available with the name {alias} in `artifact_type` group."
+            )
+
+    return mod  # type: ignore
