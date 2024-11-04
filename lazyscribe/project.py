lazyscribe/lazyscribe

commit 5ed018be81b22818c230457016e29c66d5fc8448

Address p.25 of #28 (#67)

lazyscribe/project.py
@@ -223,7 +223,7 @@ class Project:
         The new project will inherit the current project ``fpath``,
         ``author``, and ``mode``.
 
-        For details on the merging process, see :doc:`here<../how-to/merge>`.
+        For details on the merging process, see :doc:`here<../how-to/merge#updating>`.
 
         Returns
         -------
@@ -252,6 +252,8 @@ class Project:
 
     def append(self, other: Experiment):
         """Append an experiment to the project.
+
+        For details on the merging process, see :doc:`here<../how-to/merge#appending>`.
 
         Parameters
         ----------

commit f5bc8227d119c4670578a58f35685fd90fedcd9a

Fix Project Updating and Appending links
Fixes: 5ed018be81b22818c230457016e29c66d5fc8448
Signed-off-by: Alexander Kurakin <kuraga333@mail.ru>

lazyscribe/project.py
@@ -223,7 +223,7 @@ class Project:
         The new project will inherit the current project ``fpath``,
         ``author``, and ``mode``.
 
-        For details on the merging process, see :doc:`here<../how-to/merge#updating>`.
+        For details on the merging process, see :ref:`here <Project Updating>`.
 
         Returns
         -------
@@ -253,7 +253,7 @@ class Project:
     def append(self, other: Experiment):
         """Append an experiment to the project.
 
-        For details on the merging process, see :doc:`here<../how-to/merge#appending>`.
+        For details on the merging process, see :ref:`here <Project Appending>`.
 
         Parameters
         ----------