49b5f7c0777a3e10257856f6aa971f5abfd17a3b
Akshay Gupta <30963644+ak-gupta@users.noreply.github.com>
2025-10-06 17:19:13 -0400

    refactor: removing the yaml artifact handler (#158)

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -6,12 +6,10 @@ from unittest.mock import Mock, patch
 
 import pytest
 import time_machine
-import yaml
 
 from lazyscribe.artifacts import _get_handler
 from lazyscribe.artifacts.json import JSONArtifact
 from lazyscribe.artifacts.pickle import PickleArtifact
-from lazyscribe.artifacts.yaml import YAMLArtifact
 
 
 @time_machine.travel(
@@ -39,85 +37,6 @@ def test_json_handler(tmp_path):
         out = handler.read(buf)
 
     assert data == out
-
-
-@time_machine.travel(
-    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")), tick=False
-)
-@pytest.mark.parametrize(
-    "data,Loader",
-    (
-        ([{"key": "value"}], yaml.SafeLoader),
-        ([{"key": "value"}], yaml.FullLoader),
-        ([{"type": float}], yaml.FullLoader),
-        ({"key": "value", "type": str}, yaml.FullLoader),
-    ),
-)
-def test_yaml_handler(data, Loader, tmp_path):
-    """Test reading and writing YAML files with the handler."""
-    location = tmp_path / "my-location"
-    location.mkdir()
-    handler = YAMLArtifact.construct(name="My output file")
-
-    assert (
-        handler.fname
-        == f"my-output-file-{datetime.now().strftime('%Y%m%d%H%M%S')}.yaml"
-    )
-
-    with open(location / handler.fname, "w") as buf:
-        handler.write(data, buf)
-
-    assert (location / handler.fname).is_file()
-
-    with open(location / handler.fname) as buf:
-        out = handler.read(buf, Loader=Loader)
-
-    assert data == out
-
-
-@time_machine.travel(
-    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")), tick=False
-)
-def test_yaml_handler_defaults_to_safeloader(tmp_path):
-    """Test YAML handler defaults to safe loader."""
-    location = tmp_path / "my-location"
-    location.mkdir()
-
-    data = [{"key": "value"}]
-    handler = YAMLArtifact.construct(name="My output file")
-
-    assert (
-        handler.fname
-        == f"my-output-file-{datetime.now().strftime('%Y%m%d%H%M%S')}.yaml"
-    )
-
-    with open(location / handler.fname, "w") as buf:
-        handler.write(data, buf)
-
-    assert (location / handler.fname).is_file()
-
-    with open(location / handler.fname) as buf:
-        out = handler.read(buf)
-
-    assert data == out
-
-    # Test that it doesn't unserialize objects that need
-    # full loader
-    data = [{"type": float}]
-    handler = YAMLArtifact.construct(name="Unreadable")
-
-    assert handler.fname == f"unreadable-{datetime.now().strftime('%Y%m%d%H%M%S')}.yaml"
-
-    with open(location / handler.fname, "w") as buf:
-        handler.write(data, buf)
-
-    assert (location / handler.fname).is_file()
-
-    with (
-        open(location / handler.fname) as buf,
-        pytest.raises(yaml.constructor.ConstructorError),
-    ):
-        handler.read(buf)
 
 
 @time_machine.travel(
