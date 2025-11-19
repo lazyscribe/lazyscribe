56266cb8a936f60100b8781501315c5457f0b655
Akshay Gupta <akgcodes@gmail.com>
2025-11-19 21:37:43 +0000

    feat: adding a method for finding a particular release

--- lazyscribe/release.py
+++ lazyscribe/release.py
@@ -1,9 +1,15 @@
 """Repository releases."""
 
-from attrs import Factory, asdict, define, field
+import logging
+from datetime import datetime
+from typing import Literal
 
-from lazyscribe._utils import serializer, utcnow
+from attrs import Factory, define, field
+
+from lazyscribe._utils import utcnow
 from lazyscribe.repository import Repository
+
+LOG = logging.getLogger(__name__)
 
 
 @define
@@ -22,8 +28,8 @@ class Release:
     """
 
     tag: str = field()
-    artifacts: list[tuple[str, str]] = field()
-    created_at: str = Factory(utcnow)
+    artifacts: list[tuple[str, int]] = field()
+    created_at: datetime = Factory(utcnow)
 
     def to_dict(self) -> dict[str, list[tuple[str, int]] | str]:
         """Serialize the release to a dictionary.
@@ -33,7 +39,11 @@ class Release:
         dict
             A dictionary with the release information.
         """
-        return asdict(self, value_serializer=serializer)
+        return {
+            "tag": self.tag,
+            "artifacts": self.artifacts,
+            "created_at": self.created_at.isoformat(timespec="seconds"),
+        }
 
 
 def create_release(repository: Repository, tag: str) -> Release:
@@ -63,3 +73,86 @@ def create_release(repository: Repository, tag: str) -> Release:
         latest.append((name, art.version))
 
     return Release(tag=tag, artifacts=latest)
+
+
+def find_release(
+    releases: list[Release],
+    version: str | datetime | None = None,
+    match: Literal["asof", "exact"] = "exact",
+) -> Release:
+    """Find a release based on tag or timestamp.
+
+    Parameters
+    ----------
+    releases : list[lazyscribe.release.Release]
+        The releases associated with the repository.
+    version : str | datetime, optional (default None)
+        The version to find. If a string is provided, the function will assume
+        the value corresponds to a ``tag``. If a datetime is provided, the function
+        will assume the value corresponds to a creation date. If None is provided,
+        the latest release will be returned.
+    match : {"asof", "exact"}, optional (default "exact")
+        Matching logic. Only relevant if a datetime is provided for ``version``.
+        ``exact`` will provide the release with the exact value matching ``version``.
+        ``asof`` will provide the most recent release as of the ``version`` datetime
+        provided.
+
+    Returns
+    -------
+    lazyscribe.release.Release
+        The release object.
+
+    Raises
+    ------
+    ValueError
+        Raised if the ``tag`` or ``created_at`` cannot be found with the given
+        matching logic.
+    """
+    out: Release
+
+    releases_ = sorted(releases, key=lambda x: x.created_at)
+    match (match, version):
+        case (_, None):
+            out = releases_[-1]
+        case ("exact", str() as tag):
+            try:
+                out = next(ver for ver in releases_ if ver.tag == tag)
+            except StopIteration:
+                msg = f"Cannot find release with tag '{tag}'"
+                raise ValueError(msg) from None
+        case ("exact", datetime() as created_at):
+            try:
+                out = next(ver for ver in releases_ if ver.created_at == created_at)
+            except StopIteration:
+                msg = f"Cannot find release with creation date of {created_at!s}"
+                raise ValueError(msg) from None
+        case ("asof", str()):
+            raise ValueError(
+                "Cannot perform an ``asof`` match using a tag. Please provide a "
+                "``datetime.datetime`` value to perform an ``asof`` match."
+            )
+        case ("asof", datetime() as created_at):
+            if created_at < releases_[0].created_at:
+                msg = (
+                    f"Creation date {created_at!s} predates the earliest release: "
+                    f"'{releases_[0].tag}' ({releases_[0].created_at!s})"
+                )
+                raise ValueError(msg)
+            try:
+                out = next(
+                    ver
+                    for idx, ver in enumerate(releases_)
+                    if (
+                        created_at >= ver.created_at
+                        and created_at < releases_[idx + 1].created_at
+                    )
+                )
+            except IndexError:
+                out = releases_[-1]
+        case _:
+            msg = "Invalid parameterization. Please provide ``exact`` or ``asof`` for ``match``."
+            raise ValueError(msg)
+
+    LOG.info(f"Found release '{out.tag}' ({out.created_at!s})")
+
+    return out
--- tests/test_release.py
+++ tests/test_release.py
@@ -3,10 +3,11 @@
 import zoneinfo
 from datetime import datetime
 
+import pytest
 import time_machine
 
 from lazyscribe import Repository
-from lazyscribe.release import Release, create_release
+from lazyscribe.release import Release, create_release, find_release
 
 
 def test_repository_release():
@@ -36,6 +37,11 @@ def test_repository_release():
                 ("my-features", 0),
             ],
         )
+        assert release.to_dict() == {
+            "tag": "v0.1.0",
+            "created_at": "2025-06-01T00:00:00",
+            "artifacts": [("my-data", 1), ("my-features", 0)],
+        }
 
 
 def test_repository_release_filter():
@@ -71,3 +77,100 @@ def test_repository_release_filter():
     assert len(new_.artifacts) == 2
     assert new_["my-data"] == repository._search_artifact_versions("my-data", 1)
     assert new_["my-features"] == repository._search_artifact_versions("my-features", 0)
+
+
+def test_find_release_latest():
+    """Test retrieving the latest release."""
+    releases = [
+        Release("v0.1.0", [], datetime(2025, 1, 1, 0, 0, 0)),
+        Release("v0.2.0", [], datetime(2025, 2, 1, 0, 0, 0)),
+        Release("v0.2.1", [], datetime(2025, 3, 1, 0, 0, 0)),
+    ]
+    out = find_release(releases)
+
+    assert out == releases[-1]
+
+
+def test_find_release_exact_tag():
+    """Test finding a release using the tag."""
+    releases = [
+        Release("v0.1.0", [], datetime(2025, 1, 1, 0, 0, 0)),
+        Release("v0.2.0", [], datetime(2025, 2, 1, 0, 0, 0)),
+        Release("v0.2.1", [], datetime(2025, 3, 1, 0, 0, 0)),
+    ]
+    out = find_release(releases, "v0.2.0")
+
+    assert out == releases[1]
+
+    with pytest.raises(ValueError) as excinfo:
+        find_release(releases, "v1.0.0")
+
+    assert str(excinfo.value) == "Cannot find release with tag 'v1.0.0'"
+
+
+def test_find_release_exact_date():
+    """Test finding a release with an exact creation date."""
+    releases = [
+        Release("v0.1.0", [], datetime(2025, 1, 1, 0, 0, 0)),
+        Release("v0.2.0", [], datetime(2025, 2, 1, 0, 0, 0)),
+        Release("v0.2.1", [], datetime(2025, 3, 1, 0, 0, 0)),
+    ]
+    out = find_release(releases, datetime(2025, 1, 1, 0, 0, 0))
+
+    assert out == releases[0]
+
+    with pytest.raises(ValueError) as excinfo:
+        find_release(releases, datetime(2025, 6, 1, 0, 0, 0))
+
+    assert (
+        str(excinfo.value)
+        == "Cannot find release with creation date of 2025-06-01 00:00:00"
+    )
+
+
+def test_find_release_raise_error_asof_str():
+    """Test raising an error when we try to use an asof search with a string."""
+    with pytest.raises(ValueError) as excinfo:
+        find_release([], "v0.1.0", match="asof")
+
+    assert str(excinfo.value) == (
+        "Cannot perform an ``asof`` match using a tag. Please provide a ``datetime.datetime`` value "
+        "to perform an ``asof`` match."
+    )
+
+
+def test_find_release_asof_date():
+    """Test finding a release using an asof search."""
+    releases = [
+        Release("v0.1.0", [], datetime(2025, 1, 1, 0, 0, 0)),
+        Release("v0.2.0", [], datetime(2025, 2, 1, 0, 0, 0)),
+        Release("v0.2.1", [], datetime(2025, 3, 1, 0, 0, 0)),
+    ]
+
+    # First, try retrieving a release that predates the earliest
+    with pytest.raises(ValueError) as excinfo:
+        find_release(releases, datetime(2024, 12, 15, 0, 0, 0), match="asof")
+
+    assert str(excinfo.value) == (
+        "Creation date 2024-12-15 00:00:00 predates the earliest release: "
+        "'v0.1.0' (2025-01-01 00:00:00)"
+    )
+
+    # Next, perform a successful asof search
+    out = find_release(releases, datetime(2025, 2, 15, 0, 0, 0), match="asof")
+    assert out == releases[1]
+
+    # Retrieve the latest when we go beyond the last creation date
+    out = find_release(releases, datetime(2025, 6, 1, 0, 0, 0), match="asof")
+    assert out == releases[-1]
+
+
+def test_find_release_invalid_match():
+    """Test raising an error when match is invalid."""
+    with pytest.raises(ValueError) as excinfo:
+        find_release([], "v0.1.0", match="match-this-idiot")
+
+    assert (
+        str(excinfo.value)
+        == "Invalid parameterization. Please provide ``exact`` or ``asof`` for ``match``."
+    )
