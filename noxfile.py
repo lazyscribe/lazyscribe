a8fed473422d0b96113f509c6ea294dd842c6b8b
Akshay Gupta <akgcodes@gmail.com>
2026-03-29 16:35:09 +0000

    test(project): first pass testing for concurrency handling

---
+++ noxfile.py
@@ -0,0 +1,18 @@
+"""Multi-python version testing."""
+
+import nox
+
+
+@nox.session(
+    venv_backend="uv", python=["3.10", "3.11", "3.12", "3.13", "3.13t", "3.14", "3.14t"]
+)
+def test(session: nox.Session) -> None:
+    """Run the unit testing."""
+    session.run_install(
+        "uv",
+        "sync",
+        "--extra=tests",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("pytest", "tests", "-vv")
